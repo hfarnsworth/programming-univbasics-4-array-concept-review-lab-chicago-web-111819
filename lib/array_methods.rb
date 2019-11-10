5a2b67b9236e12c1c0e7fdf7c91c95dcf8dc0189
Havilah J Farnsworth <havilahfarnsworth@gmail.com>
2019-11-10 17:28:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,10 @@
 def find_element_index(array, value_to_find)
-  counter = 0
-  while array[counter] { |index|
+  array.length.times { |index|
     if array[index] === value_to_find
       return index
     end
-    counter += 1
   }
+  return nil
 end
 
 def find_max_value(array)
